db4e14a4e6ce2216b5ab8f6b8024532feb5db596
dylantigo <dylannoeltigomo@gmail.com>
2024-09-22 15:33:40 +0100

    JetBrains Article TP finished

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -38,10 +38,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(stringResource(R.string.happy_birthday_text), from = stringResource(
-                        R.string.signature_text
-                    )
-                    )
+//                    GreetingImage(stringResource(R.string.happy_birthday_text), from = stringResource(
+//                        R.string.signature_text
+//                    )
+//                    )
+                    Article(title = stringResource(R.string.title), paragraph1 = stringResource(R.string.paragraph1), paragraph2 = stringResource(R.string.paragraph2))
                 }
             }
         }
@@ -89,11 +90,48 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
     }
 }
 
+@Composable
+fun Article (title: String, paragraph1: String, paragraph2: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose)
+    val defaultFontSize = 16.sp
+    val defaultLineHeight = 20.sp
+
+    Column( verticalArrangement = Arrangement.Top){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.FillWidth
+        )
+        Column( modifier = Modifier.padding(16.dp)){
+            Text(
+                text = title,
+                fontSize = 24.sp,
+                lineHeight = 28.sp,
+                modifier= Modifier.padding(top = 32.dp, bottom = 16.dp),
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = paragraph1,
+                fontSize = defaultFontSize,
+                lineHeight = defaultLineHeight,
+            )
+            Text(
+                text = paragraph2,
+                fontSize = defaultFontSize,
+                lineHeight = defaultLineHeight,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun BirthDayCardPreview() {
     MyApplicationTheme {
-        GreetingImage("Happy BirthDay Dylan Noel", from = "Merveille and Audrey")
+//        GreetingImage("Happy BirthDay Dylan Noel", from = "Merveille and Audrey")
+        Article(title = stringResource(R.string.title), paragraph1 = stringResource(R.string.paragraph1), paragraph2 = stringResource(R.string.paragraph2))
     }
 }
